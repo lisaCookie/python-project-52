b56ad6f335f0b38715eb216603a5b75cfbef4eac
lisa <lisavetabelova@gmail.com>
2026-02-13 17:11:44 +0300

    modified:   task_manager/settings.py         modified:   task_manager/templates/users/login.html

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'webserver').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,webserver,testserver').split(',')
 
 
 # Rollbar configuration
