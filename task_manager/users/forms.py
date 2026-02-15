a117c720df35e95b7f5908b23b7e6a736ff4a4fa
lisa <lisavetabelova@gmail.com>
2026-02-15 18:52:12 +0300

    new file:   task_manager/labels/migrations/0003_alter_label_options_alter_label_created_at_and_more.py         new file:   task_manager/statuses/migrations/0003_alter_status_options_alter_status_created_at_and_more.py         new file:   task_manager/tasks/migrations/0003_alter_task_options_alter_task_author_and_more.py         modified:   task_manager/users/forms.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,12 +1,14 @@
 # users/forms.py
+
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
+from django import forms
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']  # <-- Добавлены first_name, last_name
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
         labels = {
             'username': _('Имя пользователя'),
             'first_name': _('Имя'),
@@ -15,27 +17,30 @@ class UserRegisterForm(UserCreationForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        # Упрощенные сообщения помощи для пароля
         self.fields['password1'].help_text = _('Ваш пароль должен содержать как минимум 3 символа.')
         self.fields['password2'].help_text = _('Для подтверждения введите, пожалуйста, пароль ещё раз.')
-        
-        # Русские метки
         self.fields['password1'].label = _('Пароль')
         self.fields['password2'].label = _('Подтверждение пароля')
 
+
 class UserUpdateForm(UserChangeForm):
+    password1 = forms.CharField(
+        label=_('Пароль'),
+        widget=forms.PasswordInput(render_value=True),
+        required=False,
+        help_text=_('Raw passwords are not stored, so there is no way to see this user\'s password, but you can change the password using <a href="../password/">this form</a>.'),
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1']
         labels = {
             'username': _('Имя пользователя'),
             'email': _('Email'),
             'first_name': _('Имя'),
             'last_name': _('Фамилия'),
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Убираем помощь для пароля в форме редактирования
-        self.fields['password'].help_text = _('Raw passwords are not stored, so there is no way to see this user\'s password, but you can change the password using <a href="../password/">this form</a>.')
+        self.fields['password1'].help_text = _('')
