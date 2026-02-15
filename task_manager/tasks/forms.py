07c152e06c85c7286617e9da3d066e37b748087e
lisa <lisavetabelova@gmail.com>
2026-02-15 23:19:19 +0300

    modified:   task_manager/tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -11,19 +11,29 @@ class TaskForm(forms.ModelForm):
         required=False,
         label=_('Labels')
     )
-    
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'user', 'labels']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': _('Имя')}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': _('Описание')}),
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': _('Имя')}
+            ),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 4, 'placeholder': _('Описание')}
+            ),
             'status': forms.Select(attrs={'class': 'form-control'}),
-            'user': forms.Select(attrs={'class': 'form-control'}),
+            # строка ниже заменяется на ModelChoiceField с empty_label
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Делаем поля обязательными
         self.fields['status'].required = True
-        self.fields['user'].required = True
+        # переопределяем user, чтобы добавить пустой пункт
+        self.fields['user'] = forms.ModelChoiceField(
+            queryset=self.fields['user'].queryset,
+            required=True,
+            empty_label='---------',   # вот он – option с value=""
+            widget=forms.Select(attrs={'class': 'form-control'}),
+            label=_('Исполнитель')
+        )
